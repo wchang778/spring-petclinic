b6ca90bb57ebc0c64384b4b635d893c4ef189b16
WC <wc@google.com>
2022-11-13 18:01:51 -0500

    fixed format

--- src/test/java/org/springframework/samples/petclinic/SomeTests.java
+++ src/test/java/org/springframework/samples/petclinic/SomeTests.java
@@ -1,10 +1,13 @@
 package org.springframework.samples.petclinic;
+
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class SomeTests {
-    @Test
-    public void testMe() {
-        assertThat(1).isEqualTo(0);
-    }
+
+	@Test
+	public void testMe() {
+		assertThat(1).isEqualTo(0);
+	}
+
 }
